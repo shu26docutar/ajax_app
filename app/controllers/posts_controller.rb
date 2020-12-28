ba556ffc9ad808407dce0f6739b6a3e356be0cf1
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2020-12-28 23:31:38 +0900

    既読機能フロント部文の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,9 +15,9 @@ class PostsController < ApplicationController
         else
             post.update(checked: true)
         end
-
         item = Post.find(params[:id])
-        render json { post: item }
+        render json: { post: item }
     end
+    
     
 end
